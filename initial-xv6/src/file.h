dc38576bdc984abd450a1c4de2fbdb6f4e05d841
Ahmed Araby <ahmedaraby605@gmail.com>
2022-06-20 23:42:34 +0200

    add the xv6 code base with the 'getreadcnt' system call implemented note: detailed commit history exist in my fork of xv6 https://github.com/Ahmed-Araby/xv6-public/tree/system-calls

---
+++ initial-xv6/src/file.h
@@ -0,0 +1,50 @@
+struct file {
+  enum { FD_NONE, FD_PIPE, FD_INODE } type;
+  int ref; // reference count
+  char readable;
+  char writable;
+  struct pipe *pipe;
+  struct inode *ip;
+  uint off;
+};
+
+
+// in-memory copy of an inode
+struct inode {
+  uint dev;           // Device number
+  uint inum;          // Inode number
+  int ref;            // Reference count
+  struct sleeplock lock; // protects everything below here
+  int valid;          // inode has been read from disk?
+
+  short type;         // copy of disk inode
+  short major;
+  short minor;
+  short nlink;
+  uint size;
+  uint addrs[NDIRECT+1];
+};
+
+// table mapping major device number to
+// device functions
+struct devsw {
+  int (*read)(struct inode*, char*, int);
+  int (*write)(struct inode*, char*, int);
+};
+
+extern struct devsw devsw[];
+
+/**
+ * @brief this struct hold the counter variable 
+ * that will store the number of times the fileread()
+ * get called. 
+ * also it holds the spinlock used to synchronize 
+ * the access to the counter. 
+ */
+struct readcallscnt{
+  struct spinlock lock;
+  int counter;
+};
+extern struct readcallscnt readcallscnt;
+
+#define CONSOLE 1
---
+++ initial-xv6/src/getreadcount_test.d
@@ -0,0 +1,2 @@
+getreadcount_test.o: getreadcount_test.c /usr/include/stdc-predef.h \
+ types.h user.h
